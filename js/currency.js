23c5832793791c2e67f37cc8b38c6f921df44834
Abed-Jarrah <abedjarrah97@gmail.com>
2026-06-14 20:10:56 +0300

    Improve mobile layout and localized feedback

--- js/currency.js
+++ js/currency.js
@@ -1,11 +1,17 @@
 const MezanCurrency = (() => {
-  const symbols = {
+  const arabicSymbols = {
     QAR: 'ر.ق', SAR: 'ر.س', AED: 'د.إ', KWD: 'د.ك', BHD: 'د.ب',
     OMR: 'ر.ع', JOD: 'د.أ', USD: '$', EUR: '€', TRY: '₺'
   };
-  const currencies = Object.keys(symbols);
+  const englishSymbols = {
+    QAR: 'QAR', SAR: 'SAR', AED: 'AED', KWD: 'KWD', BHD: 'BHD',
+    OMR: 'OMR', JOD: 'JOD', USD: '$', EUR: '€', TRY: '₺'
+  };
+  const currencies = Object.keys(arabicSymbols);
   const format = number => Number(number || 0).toLocaleString('en-US', { maximumFractionDigits: 0 });
   const display = value => MezanCalculations.number(value) ? format(MezanCalculations.number(value)) : '';
+  const symbol = (currency, language = 'ar') =>
+    (language === 'ar' ? arabicSymbols : englishSymbols)[currency] || currency;
 
-  return { symbols, currencies, format, display };
+  return { currencies, format, display, symbol };
 })();
